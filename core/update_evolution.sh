dd1caa6f12da7bfd5c6ad829ce87df182bdb3b3b
Alex <alexey@nsk21.ru>
2018-03-19 19:12:02 +0700

    A script to update evolution skin, updated version

--- core/update_evolution.sh
+++ core/update_evolution.sh
@@ -14,18 +14,24 @@ update_skin()
     chown -R diradmin:diradmin "${DIR}";
 }
 
-update_binary()
+update_script()
 {
     wget -O /root/updateda.sh https://raw.githubusercontent.com/poralix/directadmin-utils/master/core/updateda.sh;
     chmod 755 /root/updateda.sh;
+}
+
+update_binary()
+{
     /root/updateda.sh beta;
     service directadmin restart;
 }
 
 if [ "$1" == "-v" ] || [ "$1" == "-V" ]; then
+    [ -x "/root/updateda.sh" ] || update_script;
     update_binary;
     update_skin;
 else
+    [ -x "/root/updateda.sh" ] || update_script >/dev/null 2>&1;
     update_binary >/dev/null 2>&1;
     update_skin >/dev/null 2>&1;
 fi;
