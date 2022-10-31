29a1e992022be9da13c69190f3c590fb408f42e2
Alex Grebenschikov <alexey@nsk21.ru>
2022-10-31 13:45:38 +0700

    Added a script to list directadmin domains

--- misc/README.md
+++ misc/README.md
@@ -12,3 +12,24 @@
         --create-symlink=dirs  - Replace directory private_html with a symlink
         --create-symlink=no    - Create symlink private_html where it does not exist
 ```
+
+# A script to list DirectAdmin domains
+
+- da_domains.sh
+
+```
+Description:
+    This is a script to list directadmin domains with a requested from
+    DNS additional information.
+
+Usage:
+    ./da_domains.sh <options>
+
+Options:
+    --domains  - just list domains without DNS queries
+    --ns       - list domains with their nameservers
+    --mx       - list domains with their MX recordss
+    --ipv4     - list domains with their IPv4
+    --ipv6     - list domains with their IPv6
+```
+
---
+++ misc/da_domains.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+#----------------------------------------------------------------------
+# Description: A script for listing all domains from DirectAdmin
+#----------------------------------------------------------------------
+# Author: Alex Grebenschikov, www.poralix.com
+# Created at: Mon Oct 31 13:21:06 +07 2022
+# Last modified: Mon Oct 31 13:21:06 +07 2022
+# Version: 0.1 $ Mon Oct 31 13:21:06 +07 2022
+#----------------------------------------------------------------------
+# Copyright (c) 2022 Alex Grebenschikov, www.poralix.com
+#----------------------------------------------------------------------
+
+DNS_RESOLVER="1.1.1.1";
+
+die()
+{
+    echo $1;
+    exit $2;
+}
+
+usage()
+{
+    echo "#----------------------------------------------------------------------
+# Copyright (c) 2022 Alex Grebenschikov, www.poralix.com
+#----------------------------------------------------------------------
+
+Description:
+    This is a script to list directadmin domains with a requested from 
+    DNS additional information.
+
+Usage:
+    $0 <options>
+
+Options:
+    --domains  - just list domains without DNS queries
+    --ns       - list domains with their nameservers
+    --mx       - list domains with their MX recordss
+    --ipv4     - list domains with their IPv4
+    --ipv6     - list domains with their IPv6
+";
+    exit 0;
+}
+
+domains()
+{
+    for DOM in $(awk -F: {'print $1'} /etc/virtual/domainowners | sort | uniq); do echo - $DOM; done
+}
+
+nameservers()
+{
+    for DOM in $(awk -F: {'print $1'} /etc/virtual/domainowners | sort | uniq); do echo - $DOM: $(dig +short NS "${DNS_RESOLVER}" "${DOM}" | sort | xargs); done
+}
+
+ip4()
+{
+    for DOM in $(awk -F: {'print $1'} /etc/virtual/domainowners | sort | uniq); do echo - $DOM: $(dig +short A "${DNS_RESOLVER}" "${DOM}" | sort | xargs); done
+}
+
+ip6()
+{
+    for DOM in $(awk -F: {'print $1'} /etc/virtual/domainowners | sort | uniq); do echo - $DOM: $(dig +short AAAA "${DNS_RESOLVER}" "${DOM}" | sort | xargs); done
+}
+
+mx()
+{
+    for DOM in $(awk -F: {'print $1'} /etc/virtual/domainowners | sort | uniq); do echo - $DOM: $(dig +short MX "${DNS_RESOLVER}" "${DOM}" | sort | xargs); done
+}
+
+run_cmd()
+{
+    case $1 in
+        domains)
+            domains;
+        ;;
+        nameservers)
+            nameservers;
+        ;;
+        ip4)
+            ip4;
+        ;;
+        ip6)
+            ip6;
+        ;;
+        mx)
+            mx;
+        ;;
+        usage|*)
+            usage;
+        ;;
+    esac;
+}
+
+test -e "/etc/virtual/domainowners" || die "Error: Not a directadmin server?" 1;
+
+RUN="";
+
+for CMD in $@
+do
+    case $CMD in
+        --only-domains|--domains)
+            RUN="domains";
+        ;;
+        --ns)
+            RUN="nameservers";
+        ;;
+        --ip4)
+            RUN="ip4";
+        ;;
+        --ip6)
+            RUN="ip6";
+        ;;
+        --mx)
+            RUN="mx";
+        ;;
+        --help)
+            RUN="usage";
+        ;;
+    esac;
+done;
+
+run_cmd "${RUN}";
+
+exit 0;
